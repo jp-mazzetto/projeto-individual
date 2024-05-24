a136e947b66ae518db29eb4eecec7e6bee94e732
jp-mazzetto <joao.milantoni@sptech.school>
2024-05-24 15:55:24 -0300

    alterações na dash

--- projeto-com-web/src/controllers/usuarioController.js
+++ projeto-com-web/src/controllers/usuarioController.js
@@ -5,7 +5,7 @@ function autenticar(req, res) {
     var senha = req.body.senhaServer;
 
     if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu email está indefinido!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
@@ -18,7 +18,14 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            email: resultadoAutenticar[0].email,
+                            cpf: resultadoAutenticar[0].cpf,
+                            nome: resultadoAutenticar[0].nome,
+                            senha: resultadoAutenticar[0].senha,
+                         
+                        });
 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
