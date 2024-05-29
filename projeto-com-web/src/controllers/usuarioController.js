c885ed599847996db90d25245e4d9053e674070f
jp-mazzetto <joao.milantoni@sptech.school>
2024-05-28 21:23:39 -0300

    web data viz funcionando

--- projeto-com-web/src/controllers/usuarioController.js
+++ projeto-com-web/src/controllers/usuarioController.js
@@ -20,9 +20,9 @@ function autenticar(req, res) {
                         console.log(resultadoAutenticar);
                         res.json({
                             id: resultadoAutenticar[0].id,
-                            email: resultadoAutenticar[0].email,
-                            cpf: resultadoAutenticar[0].cpf,
                             nome: resultadoAutenticar[0].nome,
+                            cpf: resultadoAutenticar[0].cpf,
+                            email: resultadoAutenticar[0].email,
                             senha: resultadoAutenticar[0].senha,
                          
                         });
@@ -47,46 +47,42 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    var CPF = req.body.cpfServer
+    var cpf = req.body.cpfServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var confirmacaoSenha = req.body.confirmacaoSenhaServer;
 
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (CPF == undefined) {
-        res.status(400).send("Seu cpf está undefined!");
-    } else if (confirmacaoSenha == undefined) {
-        res.status(400).send("Sua confirmação de senha está undefined!");
-    }else{
+    } else {
 
-    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    usuarioModel.cadastrar(nome, cpf, email, senha, confirmacaoSenha)
-        .then(
-            function (resultado) {
-                res.json(resultado);
-            }
-        ).catch(
-            function (erro) {
-                console.log(erro);
-                console.log(
-                    "\nHouve um erro ao realizar o cadastro! Erro: ",
-                    erro.sqlMessage
-                );
-                res.status(500).json(erro.sqlMessage);
-            }
-        );
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, cpf, email, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
 }
-
 
 module.exports = {
     autenticar,
-    cadastrar
-}
+    cadastrar
 }
--- projeto-com-web/src/models/usuarioModel.js
+++ projeto-com-web/src/models/usuarioModel.js
@@ -10,13 +10,13 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, CPF, email, senha, confirmacaoSenha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, CPF, email, senha, confirmacaoSenha);
+function cadastrar(nome, cpf, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, cpf, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, cpf, email, senha, confirmacaoSenha) VALUES ('${nome}', '${CPF}', '${email}', '${senha}', '${confirmacaoSenha}');
+        INSERT INTO usuario (nome, cpf, email, senha) VALUES ('${nome}', '${cpf}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
