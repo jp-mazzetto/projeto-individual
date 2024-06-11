fe10a112fdb95de7ae396738876693dfa4aac6d3
jp-mazzetto <joao.milantoni@sptech.school>
2024-06-11 19:00:39 -0300

    web kit scroll bar

--- projeto-com-web/src/routes/quiz.js
+++ projeto-com-web/src/routes/quiz.js
@@ -8,9 +8,6 @@ router.get("/", function (req, res) {
     res.render("index");
 });
 
-// router.get("/verqtdTentaivas/:id", function (req, res) {
-//     quizController.verqtdTentaivas(req, res);
-// });
 
 router.post("/registrarPontos", function (req, res) {
     quizController.registrarPontos(req, res);
